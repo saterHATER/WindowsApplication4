67485361f46831f0b353342717ec6963495651b9
saterHATER <john.sater@valpo.edu>
2014-04-15 15:17:09 -0500

    Cleaner functionality
    Modularized code by adding mutliple methods, removing unused variables,
    ect.

--- WindowsApplication4/Form1.vb
+++ WindowsApplication4/Form1.vb
@@ -4,17 +4,10 @@ Imports System.Data.OleDb
 
 Public Class Form1
     Inherits System.Windows.Forms.Form
-    Dim OleConn As New OleDb.OleDbConnection
-    Dim OleReader As OleDb.OleDbDataReader
-    Dim OleReader1 As OleDb.OleDbDataReader
-    Dim sqlConn As SqlConnection
-    Dim Cmd As New SqlCommand
-    Dim myReader As SqlDataReader
-    Dim results As String
-    Dim textbox1 As New TextBox
-    Dim oriGin As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="
-    Dim secSchtuff As String = ";Persist Security Info=True;Jet OLEDB:Database Password=time2634"
-    'Dim bawsQuery = "SELECT MSysObjects.Name AS table_name FROM MSysObjects WHERE (((Left([Name],1))<>""~"") AND ((Left([Name],4))<>""MSys"") AND ((MSysObjects.Type) In (1,4,6))) order by MSysObjects.Name"
+    Dim OleConn As New OleDb.OleDbConnection    'We hate global variables but we need to persist over the whole app life
+    Dim sqlConn As SqlConnection                'must leave these connection variables as global persistant variables
+    Dim oriGin As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source="                          'ugh, these are fine as well
+    Dim secSchtuff As String = ";Persist Security Info=True;Jet OLEDB:Database Password=time2634"   'ugh, these are fine as well
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -28,7 +21,7 @@ Public Class Form1
         'Makin' a string array
         Dim nameList As New List(Of String)
 
-        sqlConn.Open()
+        sqlConn.Open() ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
         'READ FROM ACCESS DATABASE
         myCmd.FillSchema(myData, SchemaType.Source, "tblJob")
@@ -47,15 +40,12 @@ Public Class Form1
         sqlConn.Close()
     End Sub
 
-
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim dialog As New OpenFileDialog()
         dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
         dialog.Filter = "mdb files (*.mdb)|*.mdb|All files (*.*)|*.*"
         If DialogResult.OK = dialog.ShowDialog Then
-            textbox1.Text = dialog.FileName
-            OleConn.ConnectionString = oriGin & textbox1.Text & secSchtuff
+            OleConn.ConnectionString = oriGin & dialog.FileName & secSchtuff
         End If
     End Sub
 
@@ -67,58 +57,68 @@ Public Class Form1
         Catch ex As Exception
             MsgBox("oops: " & vbCrLf & ex.Message)
         End Try
-        konnekt()
+        DBmuncher()
     End Sub
 
-    Function konnekt()
+    Function DBmuncher()
         Try
             sqlConn.Open()
-            Console.WriteLine(sqlConn.State)
             OleConn.Open()
-            Console.WriteLine(OleConn.State)
-
-            'GET TABLE NAMES
-            'Dim allTableQuery As String = "SELECT * FROM INFORMATION_SCHEMA.TABLES"
-            'Dim allTableCmd As SqlDataAdapter = New SqlDataAdapter(allTableQuery, sqlConn)
-            'Dim allTableSet As New DataSet()
-            'allTableCmd.Fill(allTableSet)
-            'For Each table As DataTable In allTableSet.Tables
-            '    Dim currentTable1 As String = table.Namespace
-            'Next
             Dim sr As New System.IO.StreamReader("tableNames.txt")
+            'ITERATE OVER TABLE
             While sr.Read()
-                Dim currentTable As String = "T" & sr.ReadLine()
-                'Console.WriteLine(currentTable)
-                'ITERATE OVER TABLE
-                'Dim currentTable = "tblJob"
-                Dim OleCmd = New OleDb.OleDbCommand("SELECT * FROM " & currentTable, OleConn) '************good
-                OleReader = OleCmd.ExecuteReader()
-
-                'GO THROUGH WHOLE ROW
-                While OleReader.Read()
-                    Dim datum As String = Nothing
-                    Dim fieldNames As String = Nothing
-
-                    'GO THROUGH EACH COLUMN
-                    For columns = 0 To OleReader.FieldCount - 1
-                        datum = datum & ", '" & OleReader.Item(columns) & "'"
-                        fieldNames = fieldNames & ", " & OleReader.GetName(columns)
-                        'Console.WriteLine(datum)
-                    Next
-                    datum = datum.Substring(2, (datum.Length - 2))
-                    fieldNames = fieldNames.Substring(2, (fieldNames.Length - 2))
-                    Cmd.CommandText = "INSERT INTO " & currentTable & " (" & fieldNames & ")" & " VALUES (" & datum & ")"
-                    'Console.WriteLine(Cmd.CommandText)
-                    Cmd.Connection = sqlConn
-                    Cmd.ExecuteNonQuery()
-                    Console.ReadLine()
-                End While
+                Dim currentTable As String = "T" & sr.ReadLine() 'haven't quite fixed the readline() issue with sr
+                everyTable(currentTable)
             End While
         Catch ex As Exception
-            MsgBox("oops: " & vbCrLf & ex.Message)
+            MsgBox("Yowchers! We didn't finish!: " & vbCrLf & ex.Message)
         End Try
         OleConn.Close()
         sqlConn.Close()
+        Return Nothing
+    End Function
+
+    Function everyTable(ByVal tableName As String)
+        Try
+            Dim OleCmd = New OleDb.OleDbCommand("SELECT * FROM " & tableName, OleConn) 'grab all data from our access DB
+            Dim OleReader As OleDbDataReader = OleCmd.ExecuteReader() 'grab all data from our access DB
+            Dim rowCount As Int16 = 1
+            While OleReader.Read()
+                everyRow(OleReader, tableName, rowCount)
+                rowCount += 1
+            End While
+        Catch es As Exception
+            MsgBox("Arrg! reading" & tableName & " had a snag: " & vbCrLf & es.Message)
+        End Try
+        Return Nothing
+    End Function
+
+    Function everyRow(ByVal oleReader As OleDbDataReader, ByVal tableName As String, ByVal rowNum As Int16)
+        Try
+            Dim dataRow As String = Nothing
+            Dim fieldNames As String = Nothing
+            For columns = 0 To oleReader.FieldCount - 1
+                dataRow = dataRow & ", '" & oleReader.Item(columns) & "'"
+                fieldNames = fieldNames & ", " & oleReader.GetName(columns)
+            Next
+            writeToSQL(dataRow, tableName, fieldNames)
+        Catch eg As Exception
+            MsgBox("Darn! Reading row" & rowNum & " had a snag: " & vbCrLf & eg.Message)
+        End Try
+        Return Nothing
+    End Function
+
+    Function writeToSQL(ByVal dataRow As String, ByVal tableName As String, ByVal fieldNames As String)
+        Try
+            Dim sqlCmd As New SqlCommand
+            dataRow = dataRow.Substring(2, (dataRow.Length - 2))
+            fieldNames = fieldNames.Substring(2, (fieldNames.Length - 2))
+            sqlCmd.CommandText = "INSERT INTO " & tableName & " (" & fieldNames & ")" & " VALUES (" & dataRow & ")"
+            sqlCmd.Connection = sqlConn
+            sqlCmd.ExecuteNonQuery()
+        Catch el As Exception
+            MsgBox("Aw, Snaps! We couldn't write to SQL!: " & vbCrLf & el.Message)
+        End Try
         Return Nothing
     End Function
 
